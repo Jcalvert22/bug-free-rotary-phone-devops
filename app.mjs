de9939567b9f2afc4b0e7304f4e922f3a9b33298
Jcalvert22 <jcalvert6@una.edu>
2026-02-12 13:39:14 -0600

    Update response message in root route

--- app.mjs
+++ app.mjs
@@ -7,7 +7,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello Express 1234567')
+  res.send('Hello Express 123456789')
 })
 
 //start the server.
