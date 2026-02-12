2e9fb18aa6497f76e713e8a250abab5a4a0aa57e
Jcalvert22 <jcalvert6@una.edu>
2026-02-12 13:37:28 -0600

    Fix greeting message in app.mjs

--- app.mjs
+++ app.mjs
@@ -7,7 +7,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello Express 123457')
+  res.send('Hello Express 1234567')
 })
 
 //start the server.
